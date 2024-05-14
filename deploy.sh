2719bfff01c39f101cd522ac5122dbabef215f25
preethivinyl <pree1317@gmail.com>
2024-05-14 09:08:36 +0000

    commit project

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker login -u preethivinil -p dckr_pat_Eoo1sRzg6OWiGjxvnSm3qAABSVE
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+
+        docker tag devops-build preethivinil/dev:project
+        docker push preethivinil/dev:project
+
+if [[ $GIT_BRANCH == "origin/master" ]]; then
+
+        docker tag devops-build preethivinil/prod:project01
+        docker push preethivinil/prod:project01
+
+else
+        echo "failed"
+fi
+
